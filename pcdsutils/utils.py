e0773ce659cb57676318b9a9157dc2d5589927e1
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-02-25 20:08:25 -0800

    ENH: Added utils.get_instance_by_name.

--- pcdsutils/utils.py
+++ pcdsutils/utils.py
@@ -1,5 +1,6 @@
 import sys
 import importlib
+import inspect
 import logging
 
 logger = logging.getLogger(__name__)
@@ -35,3 +36,42 @@ def import_helper(klass):
     except AttributeError as exc:
         raise ImportError("Unable to import %s from %s" %
                           (cls, mod.__name__)) from exc
+
+
+def get_instance_by_name(klass, *args, **kwargs):
+    """
+    Instantiate a class with args and kwargs.
+
+    Parameters
+    ----------
+    klass : str or class
+        If klass is `str` then `import_helper` is invoked. Otherwise klass is
+        used.
+
+    args : tuple
+        Tuple of arguments to pass to the class
+
+    kwargs : dict
+        Named arguments to pass to te class
+
+    Returns
+    -------
+    object
+        Instance of `klass`
+    """
+    if inspect.isclass(klass):
+        k = klass
+    else:
+        try:
+            k = import_helper(klass)
+        except ImportError:
+            logger.error('Failed to import class %s', klass)
+            raise
+
+    try:
+        obj = k(*args, **kwargs)
+        return obj
+    except Exception:
+        logger.exception('Failed to instantiate object for class %s '
+                         'using %s and %s', klass, args, kwargs)
+        raise
