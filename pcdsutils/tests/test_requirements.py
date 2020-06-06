d86580269ea4a19614d5ff84ed403bd1c7942f48
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-06-05 18:10:35 -0700

    TST: Adding smoke-test for new option.

--- pcdsutils/tests/test_requirements.py
+++ pcdsutils/tests/test_requirements.py
@@ -16,8 +16,13 @@ def test_compare_requirements(repo_root):
         args=['-v', repo_root]
     )
 
+def test_compare_requirements_ignore_docs(repo_root):
+    pcdsutils.requirements._compare_requirements(
+        args=['--ignore-docs', repo_root]
+    )
 
 def test_requirements_from_conda(repo_root):
     pcdsutils.requirements._requirements_from_conda(
         args=['-v', '--dry-run', repo_root]
     )
+
