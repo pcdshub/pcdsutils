3ca7992c84347dc3f86237b4c30c94fe2a81a564
Zachary Lentz <zlentz@slac.stanford.edu>
2020-10-19 12:24:14 -0700

    MNT: switch from custom catche to functools.lru_cache

--- pcdsutils/ext_scripts.py
+++ pcdsutils/ext_scripts.py
@@ -1,3 +1,4 @@
+import functools
 import logging
 import re
 import socket
@@ -49,31 +50,15 @@ def call_script(args, timeout=None, ignore_return_code=False):
         raise
 
 
-cache = {}
-
-
-def cache_script(args, timeout=None, ignore_return_code=False):
-    key = ' '.join(args)
-    try:
-        return cache[key]
-    except KeyError:
-        output = call_script(args, timeout=timeout,
-                             ignore_return_code=ignore_return_code)
-        cache[key] = output
-        return output
-
-
-def clear_script_cache():
-    global cache
-    cache = {}
-
-
+@functools.lru_cache(maxsize=None)
 def get_hutch_name(timeout=10):
     """
     Call get_hutch_name to return the name of the current hutch.
 
     The current hutch is defined by which server we call
     this function from.
+
+    This function's output is cached.
 
     Parameters
     ----------
@@ -86,7 +71,7 @@ def get_hutch_name(timeout=10):
     hutch_name : str
     """
     script = SCRIPTS.format('latest', 'get_hutch_name')
-    name = cache_script(script, timeout=timeout)
+    name = call_script(script, timeout=timeout)
     return name.lower().strip(' \n')
 
 
@@ -126,9 +111,12 @@ def get_run_number(hutch=None, live=False, timeout=1):
     return int(run_number)
 
 
+@functools.lru_cache(maxsize=None)
 def get_ami_proxy(hutch, timeout=10):
     """
     Call procmgr to determine the lcls-I ami proxy hostname.
+
+    This function's output is cached.
 
     This works using a regex on the output text from procmgr ami status.
 
@@ -159,9 +147,9 @@ def get_ami_proxy(hutch, timeout=10):
     hutch = hutch.lower()
     cnf = CNF.format(hutch)
     procmgr = TOOLS.format('procmgr', 'procmgr')
-    output = cache_script([procmgr, 'status', cnf, 'ami_proxy'],
-                          timeout=timeout,
-                          ignore_return_code=True)
+    output = call_script([procmgr, 'status', cnf, 'ami_proxy'],
+                         timeout=timeout,
+                         ignore_return_code=True)
     for line in output.split('\n'):
         proxy_match = proxy_re.search(line)
         if proxy_match:
