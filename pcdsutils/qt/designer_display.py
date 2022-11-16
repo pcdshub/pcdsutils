7312d54c8ea14e341d604b739975835d8b14e6b5
Zachary Lentz <zlentz@slac.stanford.edu>
2022-11-16 14:09:14 -0800

    ENH: include Ken's improvements + generalize ui_dir

--- pcdsutils/qt/designer_display.py
+++ pcdsutils/qt/designer_display.py
@@ -1,30 +1,47 @@
 """
 Helper for using designer to layout GUIs.
 """
-from typing import ClassVar
+from pathlib import Path
+from typing import ClassVar, Protocol, Type, Union
 
 from qtpy.uic import loadUiType
 
 
+class _UiForm(Protocol):
+    @staticmethod  # <-- this is not entirely true, but PyQt5 uses it as such
+    def setupUi(*args):
+        ...
+
+    @staticmethod
+    def retranslateUi(*args, **kwargs):
+        ...
+
+
 class DesignerDisplay:
     """Helper class for loading designer .ui files and adding logic."""
-    filename: ClassVar[str]
 
-    def __init_subclass__(cls):
-        """Read the file when the class is created"""
-        super().__init_subclass__()
-        if cls.filename:
-            cls.ui_form, _ = loadUiType(
-                str(cls.filename)
-            )
-        else:
-            cls.ui_form = None
+    # You must set a filename, the path to your .ui file
+    filename: ClassVar[str]
+    # You can set this for your whole app, override in a subclass, or ignore it
+    ui_dir: ClassVar[Union[str, Path]]
+    # Created automatically
+    ui_form: ClassVar[Type[_UiForm]]
+
+    @classmethod
+    def _load_ui_if_needed(cls):
+        """Load the UI file on first load."""
+        if not hasattr(cls, "ui_form"):
+            try:
+                path = str(cls.ui_dir / cls.filename)
+            except AttributeError:
+                path = str(cls.filename)
+            cls.ui_form, _ = loadUiType(path)
 
     def __init__(self, *args, **kwargs):
         """Apply the file to this widget when the instance is created"""
+        self._load_ui_if_needed()
         super().__init__(*args, **kwargs)
-        if self.ui_form is not None:
-            self.ui_form.setupUi(self, self)
+        self.ui_form.setupUi(self, self)
 
     def retranslateUi(self, *args, **kwargs):
         """Required function for setupUi to work in __init__"""
@@ -45,5 +62,7 @@ class DesignerDisplay:
                 ...
             else:
                 if obj is not None:
-                    print(f"{attr}: {obj.__class__.__module__}.{obj.__class__.__name__}")
-
+                    cls = f"{obj.__class__.__module__}.{obj.__class__.__name__} "
+                    cls = cls.removeprefix("PyQt5.")
+                    cls = cls.removeprefix("las_dispersion_scan.widgets.")
+                    print(f"{attr}: {cls}")
