6b17b256f9aa4384c9f75b37483910786bb0121e
tangkong <tangkongrobert@gmail.com>
2024-05-03 10:02:50 -0700

    MNT/BLD: remove qtpyinheritance

--- pcdsutils/qt/__init__.py
+++ pcdsutils/qt/__init__.py
@@ -1,17 +1,7 @@
-from qtpyinheritance.properties import (PassthroughProperty,
-                                        ReadonlyPassthroughProperty,
-                                        forward_properties, forward_property,
-                                        get_qt_properties)
-
 from .designer_display import DesignerDisplay
 from .popbar import QPopBar
 
 __all__ = [
     "DesignerDisplay",
     "QPopBar",
-    "PassthroughProperty",
-    "ReadonlyPassthroughProperty",
-    "forward_properties",
-    "forward_property",
-    "get_qt_properties",
 ]
